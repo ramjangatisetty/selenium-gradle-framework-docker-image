3e786358d984c1f424aec4a5d8547e7d5e979a97
ramakrishnajangatisetty <letzautomate@gmail.com>
2025-07-15 06:26:44 -0500

    Initial Commit

---
+++ src/test/java/com/letzautomate/tests/Test001.java
@@ -0,0 +1,22 @@
+package com.letzautomate.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test001 {
+
+	@Test
+	public void googleSearch() {
+
+		WebDriverManager webDriverManager = WebDriverManager.chromedriver();
+		webDriverManager.setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://google.com");
+		driver.findElement(By.name("q")).sendKeys("HelloWorld");
+
+	}
+
+}
