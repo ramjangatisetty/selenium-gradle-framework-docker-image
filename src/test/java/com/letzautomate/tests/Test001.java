12668ba8d1eb7bf480686f3e96da43674c647a12
ramakrishnajangatisetty <letzautomate@gmail.com>
2025-07-15 11:07:12 -0500

    added docker-compose

--- src/test/java/com/letzautomate/tests/Test001.java
+++ src/test/java/com/letzautomate/tests/Test001.java
@@ -5,18 +5,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Test001 {
 
 	@Test(groups={"smoke"})
-	public void googleSearch() {
+	public void googleSearch() throws MalformedURLException {
 
-		WebDriverManager webDriverManager = WebDriverManager.chromedriver();
-		webDriverManager.setup();
 		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
-		WebDriver driver = new ChromeDriver(options);
+		WebDriver driver = new RemoteWebDriver(new URL("http://chrome:4444/wd/hub"), options);
 		driver.get("https://google.com");
 		driver.findElement(By.name("q")).sendKeys("HelloWorld");
 
